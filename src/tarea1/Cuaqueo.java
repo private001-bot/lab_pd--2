080cfe3d48b811f28658a66ab7e25acad441f38a
Bryan Samame <pruebaxdremov@gmail.com>
2026-04-25 11:13:05 -0500

    se implemento la clase cuaqueo en la interface cuaqueable

---
+++ src/tarea1/Cuaqueo.java
@@ -0,0 +1,9 @@
+
+package tarea1;
+
+public class Cuaqueo implements Cuaqueable {
+    @Override
+    public void cuaquear() {
+        System.out.println("Soy un verdadero pato por eso hago CUAC CUAC");
+    }
+}
